8602faf6672d40cef95a059cc9c07cca19af8dda
Ian Kennedy <iank@outlook.com>
2019-08-21 20:06:23 -0700

    Common stack puzzle....

---
+++ StringBalance/StringBalance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    static string isBalanced(string s)
+    {
+        var charStack = new Stack<char>();
+        var input = s.ToCharArray();
+        var leftChars = "[{(";
+        foreach (char c in input)
+        {
+            if (leftChars.Contains(c))
+                charStack.Push(c);
+            else
+            {
+                switch (c)
+                {
+                    case '}':
+                        if (charStack.Count == 0 || charStack.Peek() != '{')
+                            return ("NO");
+                        else
+                            charStack.Pop();
+                        break;
+                    case ']':
+                        if (charStack.Count == 0 || charStack.Peek() != '[')
+                            return ("NO");
+                        else
+                            charStack.Pop();
+                        break;
+                    case ')':
+                        if (charStack.Count == 0 || charStack.Peek() != '(')
+                            return ("NO");
+                        else
+                            charStack.Pop();
+                        break;
+                }
+            }
+
+        }
+        
+        return charStack.Count != 0 ? "NO" : "YES";
+    }
+
+    static void Main(string[] args)
+    {
+        string result = isBalanced("{[][()]}");
+    }
+}
